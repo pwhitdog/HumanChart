65ea9f6dea3b52129781ab7b85f9994562933f7a
Paul Whitmer <paul.whitmer@gmail.com>
2019-12-06 18:42:29 -0800

    Added entry test to chart.

--- test/chart.js
+++ test/chart.js
@@ -3,6 +3,12 @@ let Chart = artifacts.require('../Chart.sol')
 contract('Chart Tests', function(entries){
     let chart
     let entry
+
+    function toNumber(bigNumbers) {
+        return bigNumbers.map(function(bigNumber){
+            return bigNumber.toNumber()
+        })
+    }
     
     beforeEach(async function(){
         chart = await Chart.new("Test", "User")
@@ -14,6 +20,18 @@ contract('Chart Tests', function(entries){
         const lname = await chart.getLastName()
         expect(fname).to.equal("Test")
         expect(lname).to.equal("User")
+    })
+
+    it('Should be able to add an entry',  async function() {
+        const originalChartEentries = await chart.getEntries()
+        const originalChartEentriesLength = originalChartEentries.length
+
+        chart.addEntry(entry)
+        const chartWithAddedEntries = await chart.getEntries()
+        const chartWithAddedEntriesLength = chartWithAddedEntries.length
+
+        expect(originalChartEentries).to.not.equal(chartWithAddedEntries)
+        expect(originalChartEentriesLength + 1).to.equal(chartWithAddedEntriesLength)
     })
 })
 
