3d098fcc2c7b06aaa265e6672490037eda6c7f67
Paul Whitmer <paul.whitmer@gmail.com>
2019-12-12 17:23:28 -0800

    Added to vitals.

--- contracts/EntryTypes/Vitals.sol
+++ contracts/EntryTypes/Vitals.sol
@@ -2,10 +2,14 @@ pragma solidity ^0.5.11;
 import "../Entry.sol";
 
 contract Vitals is Entry{
-    uint date;
+    uint _date;
+    uint _tempInCelsius;
+    uint _beatsPerMinute;
 
     constructor() public {
         _description = "Vitals";
-        // date = Entry.getCurrentTime();
+        _date = now;
     }
+
+
 }
